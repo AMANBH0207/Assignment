6af00ef90b09b90d43e101c7c2a9c467bbd993a2
AMANBH0207 <amanbhardwaj052@gmail.com>
2024-02-27 00:36:26 +0530

    '2nd'

---
+++ traingle_pattern.cs
@@ -0,0 +1,60 @@
+    Console.Write("Enter Qestion Number to Get The desired program i.e. -1.for Traingle Pattern and 2.for get date from the given string : ");
+    string userInput = Console.ReadLine();
+
+    if(Convert.ToInt32(userInput)==1){
+        Console.Write("Enter the number of lines to print");
+        string RowStr = Console.ReadLine();
+         int i, j, row = Convert.ToInt32(RowStr);
+        for (i=0; i<row; i++)
+        {
+            for (j=row-i; j>1; j--)
+            {
+                Console.Write(" ");
+            }
+            for (int k=0; k<=i; k++ )
+            {
+                Console.Write(" *");
+            }
+            Console.WriteLine();
+        }
+    }
+        else if(Convert.ToInt32(userInput)==2){
+          Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        int length = input.Length;
+
+        // Iterate through the input string
+        for (int i = 0; i < length; i++)
+        {
+            // Check if the current character is a digit
+            if (char.IsDigit(input[i]))
+            {
+                string potentialDate = "";
+
+                // Construct potential date from digits
+                for (int j = i; j < length && char.IsDigit(input[j]); j++)
+                {
+                    potentialDate += input[j];
+                }
+
+                // If potential date has 8 digits, validate it
+                if (potentialDate.Length == 8)
+                {
+                    int month = int.Parse(potentialDate.Substring(0, 2));
+                    int day = int.Parse(potentialDate.Substring(2, 2));
+                    int year = int.Parse(potentialDate.Substring(4, 4));
+
+                    if (month >= 1 && month <= 12 && day >= 1 && day <= DateTime.DaysInMonth(year, month))
+                    {
+                        Console.WriteLine("Valid date: " + potentialDate);
+                    }
+                }
+            }
+        }
+    
+    
+    }
+
+    
+
